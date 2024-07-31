65d794033cdd3ce70c78ae90aa33e22d8c018660
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-31 23:43:36 +0300

    implement remote fetchBooksListView

--- lib/core/constants/end_points.dart
+++ lib/core/constants/end_points.dart
@@ -1,3 +1,5 @@
 class EndPoints {
   static const String baseUrl = 'https://www.googleapis.com/books/v1';
+  static const String fetchBooksEndPoint =
+      '$baseUrl/volumes?Filtering=free-ebooks&Filtering=free-books%20&q=programing';
 }
---
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,32 @@
+import 'package:bookly_app/core/class/api_service.dart';
+import 'package:bookly_app/core/constants/end_points.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntitiy>> fetchBooksListView();
+  Future<List<BookEntitiy>> fetchBestSellerBooksListView();
+}
+
+class HomeRemoteDataSourceImp extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImp({required this.apiService});
+
+  @override
+  Future<List<BookEntitiy>> fetchBestSellerBooksListView() {
+    // TODO: implement fetchBestSellerBooksListView
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntitiy>> fetchBooksListView() async {
+    var respons = await apiService.get(url: EndPoints.fetchBooksEndPoint);
+    List<BookEntitiy> books = [];
+
+    for (var book in respons['items']) {
+      books.add(BookModel.fromJson(book));
+    }
+    return books;
+  }
+}
