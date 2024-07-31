d0f3b142996edc0da2d64388477e00ca8cd35dc7
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-07-31 12:45:57 +0300

    create book entity

---
+++ lib/features/home/domain/entities/book_entitiy.dart
@@ -0,0 +1,14 @@
+class BookEntitiy {
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntitiy(
+      {required this.image,
+      required this.title,
+      required this.authorName,
+      required this.price,
+      required this.rating});
+}
