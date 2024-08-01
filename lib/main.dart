1a2cf87408d564dfa880d1a9e54ee2c3a6c3607e
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-01 13:31:24 +0300

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:bookly_app/app.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Hive.registerAdapter(BookEntitiyAdapter());
+
   runApp(const BooklyApp());
 }
