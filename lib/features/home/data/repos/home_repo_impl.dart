d2206cc50fe39c12a19f526e20dcea89af6ccc71
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-07 18:36:51 +0300

    HomeRepoImpl finish

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,52 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:bookly_app/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeLocalDataSource homeLocalDataSource;
+  final HomeRemoteDataSource homeRemoteDataSource;
+
+  HomeRepoImpl(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
+  @override
+  Future<Either<Failure, List<BookEntitiy>>>
+      fetchBestSellerBooksListView() async {
+    try {
+      List<BookEntitiy> booksFromLocal =
+          homeLocalDataSource.fetchBestSellerBooksListView();
+      if (booksFromLocal.isNotEmpty) {
+        return right(booksFromLocal);
+      }
+      List<BookEntitiy> books =
+          await homeRemoteDataSource.fetchBestSellerBooksListView();
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntitiy>>> fetchBooksListView() async {
+    try {
+      List<BookEntitiy> booksFromLocal =
+          homeLocalDataSource.fetchBooksListView();
+      if (booksFromLocal.isNotEmpty) {
+        return right(booksFromLocal);
+      }
+      List<BookEntitiy> books = await homeRemoteDataSource.fetchBooksListView();
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }
+  }
+}
