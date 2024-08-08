8059908425c1eab9971abacc955c0a9e6c5edff8
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-08 12:58:54 +0300

    add SimbleBlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bookly_app/app.dart';
+import 'package:bookly_app/core/class/simble_bloc_observer.dart';
 import 'package:bookly_app/core/constants/hive_boxes.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
+  Bloc.observer = SimbleBlocObserver();
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntitiyAdapter());
   await Hive.openBox<BookEntitiy>(HiveBoxes.booksListView);
