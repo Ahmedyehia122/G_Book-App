3dd842facb8eb03743142467ceb1286b8b9d8f05
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-08 12:41:55 +0300

    create BestSellerBooksListViewCubit

--- lib/features/home/presentation/manager/best_seller-books_list_view_cubit/best_seller_books_list_view_cubit.dart
+++ lib/features/home/presentation/manager/best_seller-books_list_view_cubit/best_seller_books_list_view_cubit.dart
@@ -1,9 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
+import 'package:bookly_app/features/home/domain/use_cases/fetch_best_seller_list_view_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'best_seller_books_list_view_state.dart';
 
 class BestSellerBooksListViewCubit extends Cubit<BestSellerBooksListViewState> {
-  BestSellerBooksListViewCubit() : super(BestSellerBooksListViewInitial());
+  BestSellerBooksListViewCubit(this.fetchBestSellerListViewUseCase)
+      : super(BestSellerBooksListViewInitial());
+
+  final FetchBestSellerListViewUseCase fetchBestSellerListViewUseCase;
+
+  Future<void> fetchBestSellerBooks() async {
+    emit(BestSellerBooksListViewLoadingState());
+    var result = await fetchBestSellerListViewUseCase.call();
+    result.fold(
+      (failure) {
+        emit(BestSellerBooksListViewFailureState(
+            errMessage: failure.errMessage));
+      },
+      (books) {
+        BestSellerBooksListViewSuccessState(books: books);
+      },
+    );
+  }
 }
