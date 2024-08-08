b887042b605ce2eea7a2de01e1efafe7640192e4
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-08 12:35:30 +0300

    create BooksListViewCubit

--- lib/features/home/presentation/manager/books_list_view_cubit/books_list_view_cubit.dart
+++ lib/features/home/presentation/manager/books_list_view_cubit/books_list_view_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
+import 'package:bookly_app/features/home/domain/use_cases/fetch_books_list_view_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'books_list_view_state.dart';
 
 class BooksListViewCubit extends Cubit<BooksListViewState> {
-  BooksListViewCubit() : super(BooksListViewInitial());
+  BooksListViewCubit(this.fetchBooksListViewUseCase)
+      : super(BooksListViewInitial());
+  final FetchBooksListViewUseCase fetchBooksListViewUseCase;
+
+  Future<void> fetchBooksListView() async {
+    emit(BooksListViewLoadingState());
+    var result = await fetchBooksListViewUseCase.call();
+    result.fold(
+      (failure) {
+        emit(BooksListViewFailureState(errMessage: failure.errMessage));
+      },
+      (books) {
+        emit(BooksListViewSuccessState(books: books));
+      },
+    );
+  }
 }
